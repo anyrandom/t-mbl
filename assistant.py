8326d59755bbe986938ee2e789b02cddb9b103c9
anyrandom <140530383+anyrandom@users.noreply.github.com>
2023-12-04 16:29:49 +0000

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -4,7 +4,7 @@ import os
 import openai
 from PIL import Image
 
-logo = Image.open("tmb_logo.png")
+logo = Image.open("tmb_logo.jpg")
 
 st.set_page_config(page_title="Assistant", initial_sidebar_state="auto", page_icon=logo)
 st.sidebar.image(logo)
